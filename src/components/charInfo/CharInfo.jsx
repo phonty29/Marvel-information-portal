11379d4eb6558f6b918ac09ae0939059353809ed
Adiar <datkaevadiar@gmail.com>
2023-02-23 16:54:50 +0600

    added error boundaries

--- src/components/charInfo/CharInfo.jsx
+++ src/components/charInfo/CharInfo.jsx
@@ -24,6 +24,11 @@ class CharInfo extends Component {
         }
     }
 
+    componentDidCatch(err, info) {
+        console.log(err, info);
+        this.setState({error: true});
+    }
+
     updateChar = () => {
         const {charId} = this.props;
         if (!charId) {
